e73df3ec736a63ace59aaa86505f1704a8e4c3f5
mlvzk <mlvzk@protonmail.com>
2019-04-21 18:18:43 +0200

    update st

--- pkgs/st/mlvzk.nix
+++ pkgs/st/mlvzk.nix
@@ -7,8 +7,8 @@ stdenv.mkDerivation {
   src = fetchFromGitHub {
     owner = "mlvzk";
     repo = "st";
-    rev = "a2972f5e74328990ced06e721aae3610d5f28cc2";
-    sha256 = "0ykdsq96jbkmywwl9qpmk30kwbnsqcxhrvwgnw8fhz9wag44y2l2";
+    rev = "613ca300d0ea3e67fded71110cb0253232e2e210";
+    sha256 = "1115l1pczx50dx7hg90qxyy2pdhyahybyi7hvnxnkh3y0bsnp7fn";
   };
 
   nativeBuildInputs = [ pkgconfig ];
